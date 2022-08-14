cd68f3c78006311556a766dbe7337e26e831d9ab
Leandro.carlos <65824271+leandro-carlos@users.noreply.github.com>
2022-08-14 20:19:57 -0300

    Comentei o código pra funcionar o cart

--- src/utils/Navigator.js
+++ src/utils/Navigator.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from '../pages/Home'
-import Cart from '../pages/Cart';
+// import Cart from '../pages/Cart';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +13,7 @@ export default function Navigator() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
-                <Stack.Screen name='Cart' component={Cart} options={{ headerShown: false }} />
+                {/* <Stack.Screen name='Cart' component={Cart} options={{ headerShown: false }} /> */}
             </Stack.Navigator>
         </NavigationContainer>
     )
